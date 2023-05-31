4ee4b25c5457f873f65dfe4555e31d46407f40d3
panggau <92567925+vincentzw-2809@users.noreply.github.com>
2023-05-31 11:11:46 +0700

    c1
    c1

---
+++ app/src/main/java/com/vincentzw202102278/hotelapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.vincentzw202102278.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
